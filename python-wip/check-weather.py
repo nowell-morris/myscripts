c4699791f1968913f2e46916e323603f49822a25
Nowell Morris <nowell29+github@gmail.com>
2022-08-11 16:57:18 -0700

    improved on the weather checking

--- python-wip/check-weather.py
+++ python-wip/check-weather.py
@@ -1,8 +1,9 @@
+from abc import get_cache_token
 import requests
-import pyld 
 import json
 import googlemaps
 from datetime import datetime
+import urwid
 
  
 gmaps = googlemaps.Client(key='AIzaSyCWfCDJjyo2r4L6AP4qffhC2h3Qy9EVOe0')
@@ -17,33 +18,53 @@ def getthelocation():
     addresstosearch = input("e.g. 123 N Center Street, Centerville, AL: ")
     # variable geocode_result is the returned object from Google's API
     geocode_result = gmaps.geocode(addresstosearch)
-    # now I turn the object into json
-    geocode_result_json = json.dumps(geocode_result)
-    youraddress = geocode_result_json("formatted_address")
+
+    print("Your address is:", geocode_result[0]['formatted_address'] )
+    print("Your Latitude and Longitude are:", geocode_result[0]['geometry']['location'])
+
+    return geocode_result[0]['geometry']['location']
+    #    "geometry":{
+        #   "location":{
+            #  "lat":33.37973,
+            #  "lng":-111.624673
+        #   },
 
 
-    # print(geocode_result)
-    print("Your address is:", )
-
-    #ourrequest = requests.get(currenturl)
-    #owmjsonoutput = ourrequest.json()
-    #description = owmjsonoutput.get("weather")[0].get("description")
-    # get current temp
-    #feelsliketemp = owmjsonoutput.get("main").get("feels_like")
 
 
 def gettheweather(lat_var, lon_var):
-    #noaaurl = "https://api.weather.gov/points/38.8894,-77.0352"
-    noaaurl = "https://api.weather.gov/points/33.4231,-111.5461"
-    noaaurlforecast = "https://api.weather.gov/gridpoints/PSR/177,53/forecast"
-    # lat=33.4231&lon=-111.5461
-    noaarequest = requests.get(noaaurlforecast)
-    #print(type(noaarequest))
-    noaajson = noaarequest.content
+    latitudevar = format(lat_var, '.4f')
+    longitudevar = format(lon_var, '.4f')
+    # noaaurl = "https://api.weather.gov/points/33.4231,-111.5461"
+    noaaurl = "https://api.weather.gov/points/"+latitudevar+','+longitudevar
+    getforecasturl = requests.get(noaaurl) # this gets the object into memory
+    noaajson = getforecasturl.content # here I am taking the object and turning it into a string  
+    obtainurl = json.loads(noaajson) # here I am taking the string and turning it into json
+    noaaurlforecast = obtainurl['properties']['forecast']  # this parses the json and  gets the URL for the actual forecast
+    
+    getobject = requests.get(noaaurlforecast)  # this gets the object into memory
+    forecastcontent = getobject.content        # here I am taking the object and turning it into a string
+    forecastjson = json.loads(forecastcontent) # here I am taking the string and turning it into json
+    # this is the json object: forecastjson
+    
+def exit_on_q(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
 
-    #print(noaajson)
-    noaadict = noaarequest.json()
-    print(json.dumps(noaadict))
 
+xandy = getthelocation()
+lati_var = xandy['lat']
+long_var = xandy['lng']
+gettheweather(lati_var, long_var)
 
-getthelocation()
+palette = [
+    ('banner', 'black', 'light gray'),
+    ('streak', 'black', 'dark red'),
+    ('bg', 'black', 'dark blue'),]
+
+txt = urwid.Text(('banner', u" Hello World "), align='center')
+map1 = urwid.AttrMap(txt, 'streak')
+fill = urwid.Filler(map1)
+map2 = urwid.AttrMap(fill, 'bg')
+loop = urwid.MainLoop(map2, palette, unhandled_input=exit_on_q)
+loop.run()
